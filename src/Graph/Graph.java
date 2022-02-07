02025984b872230c01d1916d649e34bb906c10c1
Babacar10 <Diattafive@gmail.com>
2022-02-07 09:59:36 -0500

    Graph changes for cost

--- src/Graph/Graph.java
+++ src/Graph/Graph.java
@@ -13,38 +13,55 @@ public class Graph<T> {
 	private class Node {
 		private T element;
 		private ArrayList<Edge> neighbors;
+		private int competitivecost;
 		
-		public Node(T e){
+		public Node(T e,int d){
 			element = e;
 			neighbors = new ArrayList<Edge>();
+			competitivecost= d;
 		}
 		
-		public void addEdge(T e, int cost) {
+		public void addEdge(T e, int tcost, int dcost) {
 			Node otherNode = nodes.get(e);
-			neighbors.add(new Edge(otherNode, cost));
+			neighbors.add(new Edge(otherNode, tcost,dcost));
 		}
 		
+		public void timecost(T e) {
+			
+		}
+		
+		public void distancecost(T e) {
+			
+		}
+		
+		public void competitioncost() {
+			
+		}
 	}
 	
 	
 	private class Edge {
 		private Node otherNode;
-		private int cost;
+		private int timecost;
+		private int distancecost;
+	
 		
-		public Edge(Node n, int c){
+		public Edge(Node n, int c,int d){
 			otherNode = n;
-			cost = c;
+			timecost = c;
+			distancecost = d;
+			
 		}
 	}
 
-	public boolean addNode(T e) {
-		nodes.put(e, new Node(e));
+	public boolean addNode(T e, int c) {
+		nodes.put(e, new Node(e,c));
 		return true;
 	}
 	
-	public boolean addEdge(T e1, T e2, int cost) {
+	public boolean addEdge(T e1, T e2, int tcost, int dcost) {
 		if (!nodes.containsKey(e1) && !nodes.containsKey(e2)) return false;
-		nodes.get(e1).addEdge(e2, cost);
+		nodes.get(e1).addEdge(e2, tcost, dcost);
 	    return true;
 	}
 
