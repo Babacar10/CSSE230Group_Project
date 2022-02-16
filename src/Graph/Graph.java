1c4b1c03eb49fc183723976b1a51627f92564225
duffdl <duffdl@rose-hulman.edu>
2022-02-16 09:16:29 -0500

    graph path drawing

--- src/Graph/Graph.java
+++ src/Graph/Graph.java
@@ -1,18 +1,20 @@
 package Graph;
 import java.util.ArrayList;
 import java.util.Hashtable;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.PriorityQueue;
 
 public class Graph<T> {
 	private Hashtable<Integer, Node> nodes;
 	
-	public Graph(){
+	public Graph() {
 		nodes = new Hashtable<Integer, Node>();	
 	}
 	
 	public Hashtable<Integer, Node> getNodes() {
 		return nodes;
 	}
-	
 	
 	public class Node {
 		private int seed;		
@@ -21,8 +23,7 @@ public class Graph<T> {
 		private int x;
 		private int y;
 		
-		
-		public Node(int seed, String teamName, int x, int y){
+		public Node(int seed, String teamName, int x, int y) {
 			this.seed = seed;
 			this.edges = new ArrayList<Edge>();
 			this.teamName = teamName;
@@ -33,22 +34,31 @@ public class Graph<T> {
 		public int getX() {
 			return this.x;
 		}
+		
 		public int getY() {
 			return this.y;
 		}
 		
+		public double distanceAsCrowFlies(Node n) {
+			return Math.sqrt(Math.pow(x + n.x, 2) + Math.pow(y + n.y, 2));
+		}
 		
-		
+		public void addEdge(T e, int tcost, int dcost) {
+			Node otherNode = nodes.get(e);
+		}
+
 		public void addEdge(int team2, int tcost, int dcost) {
 			Node otherNode = nodes.get(team2);
 			edges.add(new Edge(this, otherNode, tcost, dcost));
 		}
 		
-		public int timeCost(T e) {
-			return 0;
+		public ArrayList<Node> getNeighbors() {
+			ArrayList<Node> neighbors = new ArrayList<Node>(edges.size());
+			for (Edge e: edges) neighbors.add(e.node2);
+			return neighbors;
 		}
 		
-		public int distanceCost(T e) {
+		public int timeCost(T e) {
 			return 0;
 		}
 		
@@ -57,7 +67,6 @@ public class Graph<T> {
 		}
 	}
 	
-	
 	private class Edge {
 		private Node node1;
 		private Node node2;
@@ -65,8 +74,8 @@ public class Graph<T> {
 		private int distanceCost;
 	
 		
-		public Edge(Node n1, Node n2, int c,int d){
-			node1 = n1;
+		public Edge(Node n1, Node n2, int c, int d){
+			node1= n1;
 			node2 = n2;
 			timeCost = c;
 			distanceCost = d;
@@ -85,6 +94,7 @@ public class Graph<T> {
 		nodes.get(seed2).addEdge(seed1, tcost, dcost);
 	    return true;
 	}
+
 	public ArrayList<Node> shortestPath(int Cost, int sourceSeed, int targetSeed) {
 		// 0-distance 1-time 2-competitive
 		// connects from target to source
@@ -138,6 +148,4 @@ public class Graph<T> {
 		return returns;
 		
 	}
-
-
 }
